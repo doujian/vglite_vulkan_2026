ca14fd57656b3f152794b1ada836a5b7d3e57c42
Developer <dev@vglite.local>
2026-07-17 17:47:22 +0800

    fix(api): match vg_lite_fb_close signature to CTS (vg_lite_buffer_t* param)
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- inc/vg_lite_util.h
+++ inc/vg_lite_util.h
@@ -11,7 +11,7 @@ int vg_lite_load_raw(vg_lite_buffer_t *buffer, const char *name);
 int vg_lite_save_png(const char *name, vg_lite_buffer_t *buffer);
 int vg_lite_load_png(vg_lite_buffer_t *buffer, const char *name);
 int vg_lite_fb_open(vg_lite_buffer_t *buffer);
-void vg_lite_fb_close(void);
+void vg_lite_fb_close(vg_lite_buffer_t *buffer);
 void vg_lite_save_raw(const char *name, vg_lite_buffer_t *buffer);
 
 #ifdef __cplusplus
--- util/vg_lite_util.c
+++ util/vg_lite_util.c
@@ -207,7 +207,7 @@ int vg_lite_load_png(vg_lite_buffer_t *buffer, const char *name)
 }
 
 int vg_lite_fb_open(vg_lite_buffer_t *buffer) { (void)buffer; return 0; }
-void vg_lite_fb_close(void) {}
+void vg_lite_fb_close(vg_lite_buffer_t *buffer) { (void)buffer; }
 
 void vg_lite_save_raw(const char *name, vg_lite_buffer_t *buffer)
 {
